7808293acd2deb0bf34185b8d30726047eb8da0f
jaseel <jaseeljctcs@gmail.com>
2022-11-21 10:54:17 +0530

    for substraction

--- Calculator.java
+++ Calculator.java
@@ -5,6 +5,10 @@ public class Calculator {
         int sum=x+y;
         return sum;
     }
+    static int sub(int x,int y){
+        int sub=x-y;
+        return  sub;
+    }
     public static void main(String[] args) {
         System.out.println("Enter number 1,2,3,4.../n 1 -Addition 2- Substraction 3-Multiplication 4-Division");
         Scanner sc=new Scanner(System.in);
@@ -15,6 +19,14 @@ public class Calculator {
             Scanner sc1=new Scanner(System.in);
             Scanner sc2=new Scanner(System.in);
             add1(sc1.nextInt(),sc2.nextInt());
+            case 2:
+                System.out.println("Enter 2 numbers..");
+                Scanner sc3=new Scanner(System.in);
+                Scanner sc4=new Scanner(System.in);
+                System.out.println(sub(sc3.nextInt(),sc4.nextInt()));
+
+
+
         }
 
     }
